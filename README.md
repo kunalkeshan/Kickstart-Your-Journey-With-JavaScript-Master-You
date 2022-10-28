4414dcdb948d19c60a12c4692fc9164601f512dd
kunalkeshan <kunalkeshan12@gmail.com>
2022-10-29 05:27:38 +0530

    chore: initialized required modules

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Kickstart-Your-Journey-With-JavaScript-Master-You
+# Kickstart Your Journey With JavaScript - Master-You by Think-Digital
+
+<p align="center">
+    <img src="./assets/thumbnail.jpeg" alt="session thumbnail" width="60%" style="border-radius: 12px;" />
+</p>
+
 Explore the various facets of how JavaScript rules the world of web...
---
+++ Vanilla-JavaScript/01-basic_console.js
@@ -0,0 +1,11 @@
+// JavaScript Can be run in two ways
+//  - Using the browser (JS file must be linked to the HTML file)
+//  - Run outside of the browser using a runtime environment like Node.js
+
+// A statement is a line of instruction that does a certain activity
+// In the below line, the console statement prints to the browser console
+// or the terminal if run on node.js
+console.log('Hello, World!');
+
+// Notice how the ";" is at the end of the statement.
+// It's not compulsory, but placing it is always considered a good practice. 
